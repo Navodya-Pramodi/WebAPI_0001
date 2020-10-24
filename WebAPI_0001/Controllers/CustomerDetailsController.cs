0bbc2ceed7f84d3038f6b6a5cc521e3c94894c68
Navodya Pramodi <175063a@uom.lk>
2020-10-24 23:04:30 +0530

    RetriveCustomerDetailsServices#00002

--- WebAPI_0001/Controllers/CustomerDetailsController.cs
+++ WebAPI_0001/Controllers/CustomerDetailsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI_0001.Model;
+using WebAPI_0001.Services;
 
 namespace WebAPI_0001.Controllers
 {
@@ -14,36 +15,11 @@ namespace WebAPI_0001.Controllers
     {
         [HttpGet]
         [Route("CustomerDetails")]
-        public ActionResult GetDetails()
+        public ActionResult CustomerDetails()
         {
-            CustomerDetails a = new CustomerDetails()
-            {
-                Id = 01,
-                Old = 20,
-                Name = "Navodya",
-                Address = "Horana"
-            };
-            CustomerDetails b = new CustomerDetails()
-            {
-                Id = 02,
-                Old = 23,
-                Name = "Kaushalya",
-                Address = "Gampaha"
-            };
-
-            CustomerDetails c = new CustomerDetails()
-            {
-                Id = 03,
-                Old = 23,
-                Name = "Tharu",
-                Address = "Colombo"
-            };
-            List<CustomerDetails> e = new List<CustomerDetails>();
-            e.Add(a);
-            e.Add(b);
-            e.Add(c);
-
-            return Ok(e);
+            CustomerDetailsServices c = new CustomerDetailsServices();
+            var listOfCustomers = c.GetCustomerDetails();
+            return Ok(listOfCustomers);
         }
     }
 }
---
+++ WebAPI_0001/Services/CustomerDetailsServices.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_0001.Model;
+
+namespace WebAPI_0001.Services
+{
+    public class CustomerDetailsServices
+    {
+        public List<CustomerDetails> GetCustomerDetails()
+        {
+            CustomerDetails a = new CustomerDetails()
+            {
+                Id = 01,
+                Old = 20,
+                Name = "Navodya",
+                Address = "Horana"
+            };
+            CustomerDetails b = new CustomerDetails()
+            {
+                Id = 02,
+                Old = 23,
+                Name = "Kaushalya",
+                Address = "Gampaha"
+            };
+
+            CustomerDetails c = new CustomerDetails()
+            {
+                Id = 03,
+                Old = 23,
+                Name = "Tharu",
+                Address = "Colombo"
+            };
+            List<CustomerDetails> e = new List<CustomerDetails>();
+            e.Add(a);
+            e.Add(b);
+            e.Add(c);
+
+            return e;
+        }
+    }
+}
