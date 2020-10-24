2c30b2f28e7472de0f973b302801609b63c977bf
Navodya Pramodi <175063a@uom.lk>
2020-10-24 21:43:38 +0530

    RetriveCustomerDetails#00001

---
+++ WebAPI_0001/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_0001.Model;
+
+namespace WebAPI_0001.Controllers
+{
+    [ApiController]
+    [Route("CustomerDetails")]
+    public class CustomerDetailsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("CustomerDetails")]
+        public ActionResult GetDetails()
+        {
+            CustomerDetails a = new CustomerDetails()
+            {
+                Id = 01,
+                Old = 20,
+                Name = "Navodya",
+                Address = "Horana"
+            };
+            CustomerDetails b = new CustomerDetails()
+            {
+                Id = 02,
+                Old = 23,
+                Name = "Kaushalya",
+                Address = "Gampaha"
+            };
+
+            CustomerDetails c = new CustomerDetails()
+            {
+                Id = 03,
+                Old = 23,
+                Name = "Tharu",
+                Address = "Colombo"
+            };
+            List<CustomerDetails> e = new List<CustomerDetails>();
+            e.Add(a);
+            e.Add(b);
+            e.Add(c);
+
+            return Ok(e);
+        }
+    }
+}
