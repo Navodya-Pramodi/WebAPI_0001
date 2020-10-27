0bef1294eb1943122d6f805a6ec96693646698a7
Navodya Pramodi <175063a@uom.lk>
2020-10-27 22:28:46 +0530

    Swagger#00002

---
+++ WebAPI_0001/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_0001.Model;
+using WebAPI_0001.Services;
+
+namespace WebAPI_0001.Controllers
+{
+    [Route("CustomerDetails")]
+    [ApiController]
+    public class CustomerDetailsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("CustomerDetails")]
+        public ActionResult CustomerDetails()
+        {
+            CustomerDetailsServices c = new CustomerDetailsServices();
+            var listOfCustomers = c.GetCustomerDetails();
+            return Ok(listOfCustomers);
+        }
+    }
+}
---
+++ WebAPI_0001/Model/CustomerDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_0001.Model
+{
+    public class CustomerDetails
+    {
+        public long Id { get; set; }
+        public long Old { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
